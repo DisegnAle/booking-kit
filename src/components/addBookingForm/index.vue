7e560e686fb41792aec9a8d242c20201f0da5f40
DisegnAle <a.scaiola@gmail.com>
2021-03-08 21:54:31 +0100

    various fixes

--- src/components/addBookingForm/index.vue
+++ src/components/addBookingForm/index.vue
@@ -1,3 +1,3 @@
 <template src="./addBookingForm.html"></template>
 <script src="./addBookingForm.js"></script>
-<style src="./addBookingForm.less" scoped lang="less"></style>
+<style src="./addBookingForm.less" lang="less"></style>
